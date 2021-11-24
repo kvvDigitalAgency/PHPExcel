fd67ee3be81aeafe7dbc2de9b53e18476ccd6fe8
kvvDigitalAgency <72100069+kvvDigitalAgency@users.noreply.github.com>
2021-11-24 11:32:39 +0300

    Add files via upload

---
+++ PHPExcelDev/lib/fonts.php
@@ -0,0 +1,17 @@
+<?php
+    const _FONTS_LIBRARY = [
+        'Calibri' => '<font>
+{{textStyle}}            <sz val="{{size}}"/>
+            <color rgb="{{color}}"/>
+            <name val="Calibri"/>
+            <family val="2"/>
+            <scheme val="minor"/>
+        </font>',
+        'Times New Roman' => '<font>
+{{textStyle}}            <sz val="{{size}}"/>
+            <color rgb="{{color}}"/>
+            <name val="Times New Roman"/>
+            <family val="1"/>
+            <charset val="204"/>
+        </font>',
+    ];
